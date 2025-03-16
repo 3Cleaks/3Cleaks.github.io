9f9217065e30c741553e8dae6876279bd0baa4ef
3Cleaks <3cleaks@dona.one>
2025-03-16 20:41:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ if (codiceInserito !== codiceAccesso) {
     
     // Rimuove i pulsanti se il codice è errato
     document.querySelectorAll('.button').forEach(button => button.style.display = 'none');
+
+    document.querySelectorAll('.h3').forEach(h3 => h3.style.display = 'none');
     
     // Rimuove i link con la classe "file-link"
     document.querySelectorAll('.file-link').forEach(link => link.remove());
